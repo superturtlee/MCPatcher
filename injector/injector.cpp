8e56e907ed5a5bcb0da7053a6146f1fbb989e16e
superturtlee <160681686+superturtlee@users.noreply.github.com>
2025-11-08 17:01:35 +0800

    Change DLL path for Minecraft game mode

--- injector/injector.cpp
+++ injector/injector.cpp
@@ -140,7 +140,7 @@ int main(int argc, char* argv[]) {
     else {
         cout << "Dump Game Mode" << endl;
         target = "Minecraft.Windows.exe";
-        dllPath = "MCpatcher2.dll";
+        dllPath = "FileCopy.dll";
         dump = 1;
     }
     
